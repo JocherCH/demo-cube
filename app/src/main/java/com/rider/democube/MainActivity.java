4601c57f2f0bf67d0f380a127b1a1138ce00cb96
wubin <324821962@qq.com>
2017-10-25 16:40:43 +0800

    开始即刻点赞效果仿写

--- app/src/main/java/com/rider/democube/MainActivity.java
+++ app/src/main/java/com/rider/democube/MainActivity.java
@@ -1,7 +1,12 @@
 package com.rider.democube;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.rider.democube.praise.PraiseActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.btn_praise);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jumpView(PraiseActivity.class);
+            }
+        });
+
+    }
+
+
+    private void jumpView(Class nextView){
+        Intent intent = new Intent(this,nextView);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/rider/democube/praise/PraiseActivity.java
@@ -0,0 +1,10 @@
+package com.rider.democube.praise;
+
+import android.app.Activity;
+
+/**
+ * Created by wubin on 2017/10/25 0025.
+ */
+
+public class PraiseActivity extends Activity {
+}
